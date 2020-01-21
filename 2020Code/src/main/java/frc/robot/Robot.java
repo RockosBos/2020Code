150e7b02d4344ed912a0d9e01a4cfae624010dd1
FlintWeststeel <59623576+FlintWeststeel@users.noreply.github.com>
2020-01-21 15:48:06 -0500

    Jojo refrence cool dude
    Description

--- 2020Code/src/main/java/frc/robot/Robot.java
+++ 2020Code/src/main/java/frc/robot/Robot.java
@@ -12,18 +12,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-<<<<<<< HEAD
-=======
-import frc.robot.ColorSensor; //This is our color sensor class, 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.networktables.*;
-
-//without this the program will not add that other file, we need to do this with all other files.
->>>>>>> cc28847281b2cd414a4c359c36ebccdbb2efbe0e
 
 import frc.robot.Controller;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -33,24 +26,34 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
  * project.
  */
 public class Robot extends TimedRobot {
-    ColorSensor colorBoi = new ColorSensor();
+    public ColorSensor colorBoi = new ColorSensor();
 
-<<<<<<< HEAD
+    WPI_TalonSRX left = new WPI_TalonSRX(0);
+    //Controller left = new Controller(0);
+     Controller right = new Controller(1);
+     WPI_TalonSRX leftDrive1 = new WPI_TalonSRX(1);
+     WPI_VictorSPX leftDrive2 = new WPI_VictorSPX(2);
+     WPI_VictorSPX rightDrive1 = new WPI_VictorSPX(3);
+     WPI_TalonSRX rightDrive2 = new WPI_TalonSRX(4);
+     WPI_TalonSRX intakeLift = new WPI_TalonSRX(5);
+     WPI_TalonSRX intakeWheels = new WPI_TalonSRX(6);
+     WPI_TalonSRX lowerFeed = new WPI_TalonSRX(7);
+     WPI_TalonSRX upperFeed = new WPI_TalonSRX(8);
+     WPI_TalonSRX shooters = new WPI_TalonSRX(9);
+     WPI_TalonSRX controlWheelRotate = new WPI_TalonSRX(10);
+     WPI_TalonSRX controlWheel = new WPI_TalonSRX(11);
+     WPI_TalonSRX liftRotate = new WPI_TalonSRX(12);
+     WPI_TalonSRX lifter = new WPI_TalonSRX(13);
+     
+     
     
 
+     SpeedControllerGroup rightDrive = new SpeedControllerGroup(rightDrive1, rightDrive2);
+     SpeedControllerGroup leftDrive = new SpeedControllerGroup(leftDrive1, leftDrive2);
+     DifferentialDrive diffDrive = new DifferentialDrive(rightDrive, leftDrive);
     
     
-=======
-    // DifferentialDrive diffDrive = new DifferentialDrive(rightDrive, leftDrive);
-    // SpeedControllerGroup rightDrive = new SpeedControllerGroup(rightDrive1.victor, rightDrive2.talon);
-    //  SpeedControllerGroup leftDrive = new SpeedControllerGroup(leftDrive1.talon, leftDrive2.victor);
-
     
-    // DifferentialDrive diffDrive;
-    Controller right = new Controller(0);
-    Controller left = new Controller(1);
-    //We do need to add in motor controllers if we want to run the bot.
->>>>>>> cc28847281b2cd414a4c359c36ebccdbb2efbe0e
   
   
     //leftDrive1.talon.setInverted(true);
@@ -70,27 +73,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-
-    
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-    
-    //read values periodically
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
-    
-    //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
   }
 
   /**
@@ -179,8 +164,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-  // diffDrive.arcadeDrive(right.js.getRawAxis(1), right.js.getRawAxis(0));
-    //Motor();
+  diffDrive.arcadeDrive(right.js.getRawAxis(1), right.js.getRawAxis(0));
+  
+   
+    
 
 
   }
