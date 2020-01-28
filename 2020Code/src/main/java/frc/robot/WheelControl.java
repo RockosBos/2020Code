60e3c04820d908456a9b3cab189f52e49ff46cd8
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2020-01-28 16:45:36 -0500

    ugh

--- 2020Code/src/main/java/frc/robot/WheelControl.java
+++ 2020Code/src/main/java/frc/robot/WheelControl.java
@@ -34,36 +34,28 @@ public class WheelControl{//wheel position
         if(color != desiredColor){// left negitave right positive
             if(desiredColor == "G"){///////////////////////////////green///////////////////
                 System.out.println("Fuck");
-                if(color == "G")//green to green
-                    motor.set(0.0);
-                else if(color == "B")// blue to green
+                if(color == "B")// blue to green
                     motor.set(-ss);
                 else if(color == "Y")// yellow to green
                     motor.set(-fs);
                 else if(color == "R")//red to green
                     motor.set(ss);
             } else if(desiredColor == "R"){///////////////////////////red/////////////////////
-                if(color == "R")//red to red
-                    motor.set(0.0);
-                else if(color == "G")//green to red
+                if(color == "G")//green to red
                     motor.set(-ss);
                 else if(color == "B")//blue to red
                     motor.set(-fs);
                 else if(color == "Y")//yellow to red
                     motor.set(ss);
             } else if(desiredColor == "Y"){//////////////////////////////yellow/////////////////
-                if(color == "Y")//yellow to yellow
-                    motor.set(0.0);
-                else if(color == "B")//blue to yellow
+                if(color == "B")//blue to yellow
                     motor.set(ss);
                 else if(color == "R")//red to yellow
                     motor.set(-ss);
                 else if(color == "G")// green to yellow
                     motor.set(-fs);
             } else if(desiredColor == "B"){/////////////////////////blue////////////////////////////
-                if(color == "B")//blue to blue
-                    motor.set(0);
-                else if(color == "G")//green to blue
+                if(color == "G")//green to blue
                     motor.set(-ss);
                 else if(color == "Y")//yellow to blue
                     motor.set(ss);
@@ -75,60 +67,7 @@ public class WheelControl{//wheel position
     }
 
 
-    public void wheelPosition(String color, String desiredColor, WPI_VictorSPX motor){
-        double ss = 0.5;//slow speed
-        double fs = 0.7;//fast speed
-
-        if(color == "R")
-            color = "R";
-        else if(color == "G")
-            color = "G";
-        else if(color == "Y")
-            color = "Y";
-        else if(color == "B")
-            color = "B";
-
-        
-        if(color != desiredColor){// left negitave right positive
-            if(desiredColor == "G"){///////////////////////////////green///////////////////
-                if(color == "G")//green to green
-                    motor.set(0.0);
-                else if(color == "B")// blue to green
-                    motor.set(-ss);
-                else if(color == "Y")// yellow to green
-                    motor.set(-fs);
-                else if(color == "R")//red to green
-                    motor.set(ss);
-            } else if(desiredColor == "R"){///////////////////////////red/////////////////////
-                if(color == "R")//red to red
-                    motor.set(0.0);
-                else if(color == "G")//green to red
-                    motor.set(-ss);
-                else if(color == "B")//blue to red
-                    motor.set(-fs);
-                else if(color == "Y")//yellow to red
-                    motor.set(ss);
-            } else if(desiredColor == "Y"){//////////////////////////////yellow/////////////////
-                if(color == "Y")//yellow to yellow
-                    motor.set(0.0);
-                else if(color == "B")//blue to yellow
-                    motor.set(ss);
-                else if(color == "R")//red to yellow
-                    motor.set(-ss);
-                else if(color == "G")// green to yellow
-                    motor.set(-fs);
-            } else if(desiredColor == "B"){/////////////////////////blue////////////////////////////
-                if(color == "B")//blue to blue
-                    motor.set(0);
-                else if(color == "G")//green to blue
-                    motor.set(-ss);
-                else if(color == "Y")//yellow to blue
-                    motor.set(ss);
-                else if(color == "R")//red to blue
-                    motor.set(fs);
-            } else motor.set(0.0);
-
-        }
-    }
+   
+    
 }
 
