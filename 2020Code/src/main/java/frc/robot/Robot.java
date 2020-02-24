a0886225e4146914a11b2848a8dc681e6c365a81
FlintWeststeel <59623576+FlintWeststeel@users.noreply.github.com>
2020-02-24 15:52:49 -0500

    Updated Constants

--- 2020Code/src/main/java/frc/robot/Robot.java
+++ 2020Code/src/main/java/frc/robot/Robot.java
@@ -110,14 +110,14 @@ public class Robot extends TimedRobot {
         boolean value = false;
      }
      class Constants{
-         static final double INTAKE_LIFT_SPEED = 1.0;
-         static final double INTAKE_WHEELS_SPEED = 1.0;
+         static final double INTAKE_LIFT_SPEED = .5;
+         static final double INTAKE_WHEELS_SPEED = .75;
          static final double UPPER_FEED_INTAKE_SPEED = 0.7;
-         static final double UPPER_FEED_SHOOTER_SPEED = 0.8;
-         static final double LOWER_FEED_SPEED = -1.0;
+         static final double UPPER_FEED_SHOOTER_SPEED = 0.6;
+         static final double LOWER_FEED_SPEED = 1.0;
          static final double SHOOTER_ROTATE_SLOW_SPEED = 0.1;
          static final double SHOOTER_ROTATE_FAST_SPEED = 0.4;
-         static final double SHOOTER_SPEED = -0.8;
+         static final double SHOOTER_SPEED = -0.92;
          static final double CONTROL_WHEEL_ROTATE_SPEED = 0.1;
          static final double CONTROL_WHEEL_WHEEL_SPEED = 0.1;
          static final double LIFT_ROTATE_SPEED = 0.1;
